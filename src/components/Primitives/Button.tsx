c849dee55c0998fd0a75ca6a565b4160b16fbbbc
Dantorori <dantorori2234@gmail.com>
2025-01-02 14:57:15 +0400

    some changes

--- src/components/Primitives/Button.tsx
+++ src/components/Primitives/Button.tsx
@@ -12,37 +12,51 @@ interface buttonProps
 }
 
 const PrimaryButton = styled.button<buttonProps>`
-  background-color: ${(props) =>
-    props.$danger
-      ? props.theme.colors.danger[500]
-      : props.$warning
-        ? "#FACC15"
-        : props.$success
-          ? "#4ADE80"
-          : props.theme.colors.brand[500]};
-  color: white;
   border-radius: 0.5rem;
   font-size: 1rem;
   padding: 0.4rem 0.9rem;
   cursor: pointer;
+
+  background-color: ${(props) =>
+    props.$danger
+      ? props.theme.colors.danger[500]
+      : props.$warning
+        ? props.theme.colors.warning[500]
+        : props.$success
+          ? props.theme.colors.success[500]
+          : props.theme.colors.brand[500]};
+
+  color: white;
+
   &:hover:not(:disabled) {
-    background-color: ${(props) => (props.$danger ? "#dc2626" : "#2570eb")};
+    background-color: ${(props) =>
+      props.$danger
+        ? props.theme.colors.danger[600]
+        : props.$warning
+          ? props.theme.colors.warning[600]
+          : props.$success
+            ? props.theme.colors.success[600]
+            : "#2570eb"};
   }
+
   &:active:not(:disabled) {
-    background-color: ${(props) => (props.$danger ? "#b91c1c" : "#1d64d8")};
+    background-color: ${(props) =>
+      props.$danger
+        ? props.theme.colors.danger[700]
+        : props.$warning
+          ? props.theme.colors.warning[700]
+          : props.$success
+            ? props.theme.colors.success[700]
+            : "#1d64d8"};
   }
+
   &:focus:not(:disabled) {
-    outline: 1px solid ${(props) => (props.$danger ? "#ef4444" : "#3b82f6")};
+    outline: 1px solid
+      ${(props) => (props.$danger ? props.theme.colors.danger[500] : "#3b82f6")};
     outline-offset: 2px;
   }
 `;
 
-const SecondaryButton = styled.button<buttonProps>``;
-
-export default function Button({ children, secondary, ...props }: buttonProps) {
-  if (secondary) {
-    <SecondaryButton {...props}>{children}</SecondaryButton>;
-  } else {
-    return <PrimaryButton {...props}>{children}</PrimaryButton>;
-  }
+export default function Button({ children, ...props }: buttonProps) {
+  return <PrimaryButton {...props}>{children}</PrimaryButton>;
 }
